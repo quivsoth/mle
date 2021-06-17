aab44f9251e4a754de592c35923114a5faf699ae
quivsoth <phillip.knezevich@gmail.com>
2021-06-17 15:52:35 -0400

    updated new images added collections

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('shop/index', { title: 'Baja La Bruja - ' });
 });
 
+
+/* GET home page. */
+router.get('/collections', function(req, res, next) {
+  res.render('shop/collections', { title: 'Baja La Bruja - Collections' });
+});
+
+
+
 module.exports = router;
