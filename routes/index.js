baca1e25a9bd36e05abdcfa9d8c81af4f449b097
quivsoth <phillip.knezevich@gmail.com>
2021-06-18 17:12:42 -0400

    added db components

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Collection = require('../models/collections');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Baja La Bruja - ' });
+  console.log('getting');
+  Collection.find(function(err, docs) {
+      console.log('found!  : ' + docs);
+
+  });
+  var collection101  = Collection.find();
+  res.render('shop/index', { title: 'Baja La Bruja - ', collections: collection101 });
 });
 
 
 /* GET home page. */
 router.get('/collections', function(req, res, next) {
-  res.render('shop/collections', { title: 'Baja La Bruja - Collections' });
+  var collections = Collection.find();
+  res.render('shop/collections', { title: 'Baja La Bruja - Collections', collections: collections });
 });
 
 
