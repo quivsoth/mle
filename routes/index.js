2c67659bd3ed89ae1d52fe1d8a14e7ff66a58ef3
PK <pksubscriber@gmail.com>
2022-06-23 23:40:02 -0400

    moved messages to all pages

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const { createServer } = require('livereload');
-const { registerHelper } = require('hbs');
+const {createServer} = require('livereload');
+const {registerHelper} = require('hbs');
 const {MongoClient} = require('mongodb');
 const uri = process.env.MONGO_DB;
 
@@ -9,242 +9,325 @@ var router = express.Router();
 var Cart = require('../models/cart');
 var Collection = require("../models/collection");
 
-const { deserializeUser } = require('passport');
+const {deserializeUser} = require('passport');
+
+
 // var csrf = require('csurf');
 // var csrfProtection = csrf();
 // router.use(csrfProtection);
 
 
-router.get('/sp', function(req, res, next) {  
-  res.render("site/scratchpad");
-}); 
+router.get('/sp', function (req, res, next) {
+    res.render("site/scratchpad");
+});
 
 
 /*    Description: View for HOME page.
       Method: GET                     */
-router.get('/', function(req, res, next) {
-  res.render('site/index', { title: 'Baja La Bruja - Fighting Fast Fashion'});
+router.get('/', function (req, res, next) {
+  var messages = req.flash('info');
+  res.render('site/index', {title: 'Baja La Bruja - Fighting Fast Fashion', 
+    messages: messages,
+    hasMessages: messages.length > 0});
 });
 
 /*    Description: View for lifestyle page.
       Method: GET                     */
-router.get('/lifestyle', function(req, res, next) {
-  res.render('site/lifestyle', { title: 'Baja La Bruja - F#$ck Fast Fashion'});
+router.get('/lifestyle', function (req, res, next) {
+    var messages = req.flash('info');  
+    res.render('site/lifestyle', {title: 'Baja La Bruja - F#$ck Fast Fashion', messages: messages,
+    hasMessages: messages.length > 0});
 });
 
 /*    Description: View for mixed media page.
       Method: GET                     */
-router.get('/mixedmedia', function(req, res, next) {
-  res.render('site/mixedmedia', { title: 'Baja La Bruja - Mixed Media'});
+router.get('/mixedmedia', function (req, res, next) {
+  var messages = req.flash('info');  
+  res.render('site/mixedmedia', {title: 'Baja La Bruja - Mixed Media', messages: messages,
+    hasMessages: messages.length > 0});
 });
 
 /*    Description: View for Gift page.
       Method: GET                     */
-router.get('/gifts', function(req, res, next) {
-  res.render('site/gifts', { title: 'Baja La Bruja - Gifts'});
+router.get('/gifts', function (req, res, next) {
+  var messages = req.flash('info');
+  res.render('site/gifts', {title: 'Baja La Bruja - Gifts', messages: messages,
+  hasMessages: messages.length > 0});
 });
 
 /*    Description: View for La Bruja page.
       Method: GET                     */
-router.get('/labruja', function(req, res, next) {
-  res.render('site/labruja', { title: 'Baja La Bruja - La Bruja'});
+router.get('/labruja', function (req, res, next) {
+    var messages = req.flash('info');
+    res.render('site/labruja', {title: 'Baja La Bruja - La Bruja', messages: messages,
+    hasMessages: messages.length > 0});
 });
 
 /*    Description: View for heritage page.
       Method: GET                     */
-router.get('/heritage', function(req, res, next) {
-  res.render('site/heritage', { title: 'Baja La Bruja - La Bruja'});
+router.get('/heritage', function (req, res, next) {
+  var messages = req.flash('info');
+    res.render('site/heritage', {title: 'Baja La Bruja - La Bruja', messages: messages,
+    hasMessages: messages.length > 0});
 });
 
 /*    Description: View for Contact page.
       Method: GET                     */
-router.get('/contacts', function(req, res, next) {
-  res.render('site/contacts', { title: 'Baja La Bruja - Contact Us'});
+router.get('/contacts', function (req, res, next) {
+  var messages = req.flash('info');
+    res.render('site/contacts', {title: 'Baja La Bruja - Contact Us', messages: messages,
+    hasMessages: messages.length > 0});
 });
 
 /*    Description: View for Collections page.
       Method: GET                     */
-router.get('/collections', function(req, res, next) {
-  (async function() {
-    const collections = await getCollections();
-    var productChunks = [];
-    var chunkSize = 5;
-    for (let i = 0; i < collections.length; i+= chunkSize) {
-      productChunks.push(collections.slice(i, i + chunkSize));
-    }
-    res.render('shop/collections', { title: 'Baja La Bruja - Collections', collections: productChunks });
-  })();
+router.get('/collections', function (req, res, next) {
+    (async function () {
+      var messages = req.flash('info');
+        const collections = await getCollections();
+        var productChunks = [];
+        var chunkSize = 5;
+        for (let i = 0; i < collections.length; i += chunkSize) {
+            productChunks.push(collections.slice(i, i + chunkSize));
+        }
+        res.render('shop/collections', {
+            title: 'Baja La Bruja - Collections',
+            collections: productChunks,
+            messages: messages,
+            hasMessages: messages.length > 0
+        });
+    })();
 });
 
 /*      Description: Products in Collection page.
         Method: GET                    */
-router.get('/products', function(req, res, next) {
-  (async function() {
-    let collectionId = req.query.collectionId;
-    const products = await getProducts(collectionId);
+router.get('/products', function (req, res, next) {
+    (async function () {
+      var messages = req.flash('info');
+        let collectionId = req.query.collectionId;
+        const products = await getProducts(collectionId);
 
-    var activeProducts = [];
-    for (let i = 0; i < products.products.length; i++) {
-      if(products.products[i].active === true) activeProducts.push(products.products[i]);
-    }
+        var activeProducts = [];
+        for (let i = 0; i < products.products.length; i++) {
+            if (products.products[i].active === true) 
+                activeProducts.push(products.products[i]);            
+        }
 
-    var productChunks = [];
-    var chunkSize = 5;
-    for (let i = 0; i < activeProducts.length; i+= chunkSize) {
-      productChunks.push(activeProducts.slice(i, i + chunkSize));
-    }
-    res.render('shop/products', { title: 'Baja La Bruja - Products', products: productChunks, breadCrumb: products.collectionName, collectionId: collectionId });
-  })();
+        var productChunks = [];
+        var chunkSize = 5;
+        for (let i = 0; i < activeProducts.length; i += chunkSize) {
+            productChunks.push(activeProducts.slice(i, i + chunkSize));
+        }
+        res.render('shop/products', {
+            title: 'Baja La Bruja - Products',
+            products: productChunks,
+            breadCrumb: products.collectionName,
+            collectionId: collectionId,
+            messages: messages,
+            hasMessages: messages.length > 0
+        });
+    })();
 });
 
 /*      Description: Item/Product Detail View.
         Method: GET                     */
-router.get('/item', function(req, res, next) {
-  (async function() {
-    let itemId = req.query.itemId;
-    let collectionId = req.query.collectionId;
-    let inCart = false;
-    if (req.session.hasOwnProperty('cart') && req.session.cart.items[itemId]) {
-      inCart = true;
-    }
-    // if(req.session.cart.items[itemId]) inCart = true;
-    const item = await getItem(itemId, collectionId);
-    res.render('shop/item', { title: 'Baja La Bruja - Items', item: item.item, collectionId: collectionId, collectionName: item.collectioName, inCart: inCart});
-  })();
+router.get('/item', function (req, res, next) {
+    (async function () {
+      var messages = req.flash('info');
+        let itemId = req.query.itemId;
+        let collectionId = req.query.collectionId;
+        let inCart = false;
+        if (req.session.hasOwnProperty('cart') && req.session.cart.items[itemId]) {
+            inCart = true;
+        }
+        // if(req.session.cart.items[itemId]) inCart = true;
+        const item = await getItem(itemId, collectionId);
+        res.render('shop/item', {
+            title: 'Baja La Bruja - Items',
+            item: item.item,
+            collectionId: collectionId,
+            collectionName: item.collectioName,
+            inCart: inCart,
+            messages: messages,
+            hasMessages: messages.length > 0 
+        });
+    })();
 });
 
 /*      Description: Item/Product Detail View.
         Method: GET                     */
-router.get('/item_admin', function(req, res, next) {
-  (async function() {
-    let itemId = req.query.itemId;
-    let collectionId = req.query.collectionId;
-    let inCart = false;
-    if (req.session.hasOwnProperty('cart') && req.session.cart.items[itemId]) {
-      inCart = true;
-    }
-    const item = await getItem(itemId, collectionId);
-    var messages = req.flash('info');
-    res.render('shop/item_admin', { title: 'Baja La Bruja - Items', item: item.item, collectionId: collectionId, collectionName: item.collectioName, inCart: inCart, messages: messages, hasMessages: messages.length > 0});
-  })();
+router.get('/item_admin', function (req, res, next) {
+    (async function () {
+      var messages = req.flash('info');
+        let itemId = req.query.itemId;
+        let collectionId = req.query.collectionId;
+        let inCart = false;
+        if (req.session.hasOwnProperty('cart') && req.session.cart.items[itemId]) {
+            inCart = true;
+        }
+        const item = await getItem(itemId, collectionId);
+        var messages = req.flash('info');
+        res.render('shop/item_admin', {
+            title: 'Baja La Bruja - Items',
+            item: item.item,
+            collectionId: collectionId,
+            collectionName: item.collectioName,
+            inCart: inCart,
+            messages: messages,
+            hasMessages: messages.length > 0
+        });
+    })();
 });
 
 /*      Description: Add item to Shopping Cart.
         Method: GET                            */
-router.get('/addCart', function(req, res, next) {
-  (async function() {
-    let productId = req.query.productId;
-    let collectionId = req.query.collectionId;
-    var cart = new Cart(req.session.cart ? req.session.cart : {});
-    const item = await getItem(productId, collectionId);
-    cart.add(item.item, item.item.productId);
-    req.session.cart = cart;
-    res.redirect('/item?itemId=' + productId + "&collectionId="+collectionId);
-  })();
+router.get('/addCart', function (req, res, next) {
+    (async function () {
+      var messages = req.flash('info');
+        let productId = req.query.productId;
+        let collectionId = req.query.collectionId;
+        var cart = new Cart(req.session.cart ? req.session.cart : {});
+        const item = await getItem(productId, collectionId);
+        cart.add(item.item, item.item.productId);
+        req.session.cart = cart;
+        res.redirect('/item?itemId=' + productId + "&collectionId=" + collectionId);
+    })();
 });
 
 /*      Description: Delete item in Shopping Cart.
         Method: GET                           */
-router.get('/deleteCart', function(req, res, next) {
-  (async function() {
-    let productId = req.query.productId;
-    var cart = new Cart(req.session.cart);
-    cart.delete(productId);
-    req.session.cart = cart;
-    res.redirect('/shopping-cart', );
-  })();
+router.get('/deleteCart', function (req, res, next) {
+    (async function () {
+      var messages = req.flash('info');
+        let productId = req.query.productId;
+        var cart = new Cart(req.session.cart);
+        cart.delete(productId);
+        req.session.cart = cart;
+        res.redirect('/shopping-cart',);
+    })();
 });
 
 /*      Description: View Shopping Cart.
         Method: GET                           */
-router.get('/shopping-cart', function(req, res, next) {
-  (async function() {
-    if(!req.session.cart) {
-      return res.render('shop/shopping-cart', {products: null});
-    }
-    var cart = new Cart(req.session.cart);
-    console.log(req.session.cart);
-    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
-  })();
+router.get('/shopping-cart', function (req, res, next) {
+    (async function () {
+      var messages = req.flash('info');
+        if (! req.session.cart) {
+            return res.render('shop/shopping-cart', {products: null});
+        }
+        var cart = new Cart(req.session.cart);
+        console.log(req.session.cart);
+        res.render('shop/shopping-cart', {
+            products: cart.generateArray(),
+            totalPrice: cart.totalPrice,
+            messages: messages,
+            hasMessages: messages.length > 0
+        });
+    })();
 });
 
 /*      Description: Checkout of Shopping Cart.
         Method: GET                           */
-router.get('/checkout', function(req, res, next) {
-  (async function() {
-    if(!req.session.cart) {
-      return res.render('shop/checkout', {products: null});
-    }
-    var cart = new Cart(req.session.cart);
-
-    res.render('shop/checkout', {products: cart.generateArray(), totalPrice: cart.totalPrice});
-  })();
+router.get('/checkout', function (req, res, next) {
+    (async function () {
+        if (! req.session.cart) {
+            return res.render('shop/checkout', {products: null});
+        }
+        var cart = new Cart(req.session.cart);
+        var messages = req.flash('info');
+        res.render('shop/checkout', {
+            products: cart.generateArray(),
+            totalPrice: cart.totalPrice,
+            messages: messages,
+            hasMessages: messages.length > 0
+        });
+    })();
 });
 
 /*      Description: Update item in collection
         Method: PUT                           */
-router.put('/updateProduct/:collection/:id', function(req, res) {
-  (async function() {
-    let itemId = parseInt(req.params.id);
-    let collectionId = parseInt(req.params.collection);
-    await updateItem(itemId, collectionId, req.body.productName, req.body.description, req.body.price, req.body.size);
-    req.flash('info', 'Item # ' + itemId + 'has been updated');
-    res.redirect('/item_admin?itemId=' + itemId + "&collectionId="+collectionId);
+router.put('/updateProduct/:collection/:id', function (req, res) {
+    (async function () {
+        let itemId = parseInt(req.params.id);
+        let collectionId = parseInt(req.params.collection);
+        await updateItem(itemId, collectionId, req.body.productName, req.body.description, req.body.price, req.body.size);
+        req.flash('info', 'Item # ' + itemId + 'has been updated');
+        res.redirect('/item_admin?itemId=' + itemId + "&collectionId=" + collectionId);
+    })();
+});
+
+/*      Description: Update item in collection
+        Method: PUT                           */
+router.put('/emailSubscribe', function (req, res) {
+  (async function () {
+    req.flash('info', 'Thank you for subscribing!');
+    var referrer = req.headers['referer'];
+    res.redirect(referrer);
   })();
 });
 
 module.exports = router;
 
-
-
-
 // -*-*-*-*-*-*-*-*-*-*-*-*-* DB FUNCTIONS -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*//
-async function getCollections(){
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  try {
-      await client.connect();
-      const cursor = client.db("shop").collection("bruja").find({active: true}).sort({collectionId:1});
-      const results = await cursor.toArray();
-      return results;
-  } catch (e) { console.error(e); }
-  finally { await client.close(); }
+async function getCollections() {
+    const client = new MongoClient(uri, {useUnifiedTopology: true});
+    try {
+        await client.connect();
+        const cursor = client.db("shop").collection("bruja").find({active: true}).sort({collectionId: 1});
+        const results = await cursor.toArray();
+        return results;
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
 
 async function getProducts(collectionId) {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  try {
-      await client.connect();
-      const cursor = await client.db("shop").collection("bruja").findOne({collectionId: Number(collectionId), active: true});
-      return cursor;
-  } catch (e) { console.error(e); }
-  finally { await client.close(); }
+    const client = new MongoClient(uri, {useUnifiedTopology: true});
+    try {
+        await client.connect();
+        const cursor = await client.db("shop").collection("bruja").findOne({collectionId: Number(collectionId), active: true});
+        return cursor;
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
 
-async function getItem(productId, collectionId){
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  try {
-      await client.connect();
-      var result = {};
-      const cursor = await client.db("shop").collection("bruja").findOne({"collectionId": parseInt(collectionId)});
-      result.collectioName = cursor.collectionName;
-      let item = cursor.products.find(product => product.productId == productId);
-      result.item = item;
-      return result;
-  } catch (e) { console.error(e); }
-  finally { await client.close(); }
+async function getItem(productId, collectionId) {
+    const client = new MongoClient(uri, {useUnifiedTopology: true});
+    try {
+        await client.connect();
+        var result = {};
+        const cursor = await client.db("shop").collection("bruja").findOne({"collectionId": parseInt(collectionId)});
+        result.collectioName = cursor.collectionName;
+        let item = cursor.products.find(product => product.productId == productId);
+        result.item = item;
+        return result;
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
 
 async function updateItem(productId, collectionId, productName, description, price, size) {
-  var query = { collectionId : collectionId }; 
-  Collection.findOne(query, function (err, result) {
-    if(err) { console.log(err); }
-    var p = result.products.filter(function (item) {
-      return item.productId === productId;
-    }).pop();
-    p.productName = productName;
-    p.description = description;
-    p.price = price;
-    p.size = size;
-    result.save();
-  });
+    var query = {
+        collectionId: collectionId
+    };
+    Collection.findOne(query, function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+        var p = result.products.filter(function (item) {
+            return item.productId === productId;
+        }).pop();
+        p.productName = productName;
+        p.description = description;
+        p.price = price;
+        p.size = size;
+        result.save();
+    });
 }
