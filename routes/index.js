3e43b0ca1bbf741a619334d87dd774d79be2bb47
PK <pksubscriber@gmail.com>
2022-06-25 02:21:33 -0400

    added shipping calculator

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const {createServer} = require('livereload');
+const https = require('https');
+const request = require('request');
 const {registerHelper} = require('hbs');
 const {MongoClient} = require('mongodb');
 const uri = process.env.MONGO_DB;
@@ -31,6 +33,76 @@ router.get('/', function (req, res, next) {
     messages: messages,
     hasMessages: messages.length > 0});
 });
+
+/*    Description: Shipping Home Page
+      Method: GET                     */
+router.get('/shipping', function (req, res, next) {
+var messages = req.flash('info');
+res.render('site/shipping', {title: 'Baja La Bruja - Shipping', 
+    messages: messages,
+    hasMessages: messages.length > 0});
+});
+
+/*    Description: Shipping Calculator
+      Method: GET                     */
+router.get('/calculator', function (req, res, next) {
+    (async function () {
+        var messages = req.flash('info');
+        var request = require('request');
+        var cost = 0, deliveryTime, service;
+        var length, width, height, weight, postCodeFrom, postCodeTo;
+        var service_code = "AUS_PARCEL_REGULAR";
+
+        length = req.body.length;
+        width = req.body.width;
+        height = req.body.height;
+        weight =req.body.weight;
+        postCodeFrom = req.body.postCodeFrom
+        postCodeTo = req.body.postCodeTo;
+
+        const australiaPostAPI = new URL("http://digitalapi.auspost.com.au/postage/parcel/domestic/calculate.json");
+        australiaPostAPI.searchParams.append("length", length);
+        australiaPostAPI.searchParams.append("width", width);
+        australiaPostAPI.searchParams.append("height", height);
+        australiaPostAPI.searchParams.append("weight", weight);
+        australiaPostAPI.searchParams.append("from_postcode", postCodeFrom);
+        australiaPostAPI.searchParams.append("to_postcode", postCodeTo);
+        australiaPostAPI.searchParams.append("service_code", service_code);
+        
+        console.log(australiaPostAPI.href);
+
+        var options = {
+            'method': 'POST',
+            'url': australiaPostAPI.href,
+            'headers': {
+                'AUTH-KEY': 'e093c2e3-d6c1-4cc7-ad2f-a85147dc3d05'
+            },
+            followAllRedirects: true
+        };
+
+        request(options, function (error, response) {
+            if (error) throw new Error(error);
+            var g = JSON.parse(response.body, (key, value) => {
+                if(key == 'total_cost') { cost = value }
+                if(key == 'delivery_time') { deliveryTime = value }
+                if(key == 'service') { service = value }
+                // console.log(key); // log the current property name, the last is "".
+                return value;     // return the unchanged property value.
+            });
+
+            res.render('site/shipping', {title: 'Baja La Bruja - Shipping', 
+                messages: messages,
+                cost: cost,
+                deliveryTime: deliveryTime,
+                service: service,
+                hasCost: cost.length > 0,
+                hasMessages: messages.length > 0
+            });
+        });
+    })();
+});
+
+
 
 /*    Description: View for lifestyle page.
       Method: GET                     */
