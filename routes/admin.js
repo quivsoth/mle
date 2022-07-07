92e3099ec37eeb9f61d20532d285b7fec32d7f67
PK <pksubscriber@gmail.com>
2022-07-07 09:46:52 -0400

    removing mailer

--- routes/admin.js
+++ routes/admin.js
@@ -55,66 +55,66 @@ router.get('/getItem', function (req, res, next) {
     //     })();
     // }); 
 
-router.get('/sendEmail', function (req, res, next) {
-    (async function () {
-        // Use at least Nodemailer v4.1.0
-const nodemailer = require('nodemailer');
+// router.get('/sendEmail', function (req, res, next) {
+//     (async function () {
+//         // Use at least Nodemailer v4.1.0
+// const nodemailer = require('nodemailer');
 
-// Generate SMTP service account from ethereal.email
-nodemailer.createTestAccount((err, account) => {
-            if (err) {
-                console.error('Failed to create a testing account. ' + err.message);
-                return process.exit(1);
-            }
+// // Generate SMTP service account from ethereal.email
+// nodemailer.createTestAccount((err, account) => {
+//             if (err) {
+//                 console.error('Failed to create a testing account. ' + err.message);
+//                 return process.exit(1);
+//             }
 
-            console.log('Credentials obtained, sending message...');
+//             console.log('Credentials obtained, sending message...');
 
-            // Create a SMTP transporter object
-            // let transporter = nodemailer.createTransport({
-            //     host: account.smtp.host,
-            //     port: account.smtp.port,
-            //     secure: account.smtp.secure,
-            //     auth: {
-            //         user: account.user,
-            //         pass: account.pass
-            //     }
-            // });
+//             // Create a SMTP transporter object
+//             // let transporter = nodemailer.createTransport({
+//             //     host: account.smtp.host,
+//             //     port: account.smtp.port,
+//             //     secure: account.smtp.secure,
+//             //     auth: {
+//             //         user: account.user,
+//             //         pass: account.pass
+//             //     }
+//             // });
 
-            let transporter = nodemailer.createTransport({
-                host: "mail.bajalabruja.org",
-                port: 2079,
-                secure: true,
-                auth: {
-                  user: "billing@bajalabruja.org",
-                  pass: "Samnmax11!"
-                },
-                tls: {
-                    rejectUnauthorized: false
-                }
-             });
+//             let transporter = nodemailer.createTransport({
+//                 host: "mail.bajalabruja.org",
+//                 port: 2079,
+//                 secure: true,
+//                 auth: {
+//                   user: "billing@bajalabruja.org",
+//                   pass: "Samnmax11!"
+//                 },
+//                 tls: {
+//                     rejectUnauthorized: false
+//                 }
+//              });
 
-            // Message object
-            let message = {
-                from: 'Sender Name <billing@bajalabruja.org>',
-                to: 'Recipient <pksubscriber@gmail.com>',
-                subject: 'Nodemailer is unicode friendly',
-                text: 'Hello to myself!',
-                html: '<p><b>Hello</b> to myself!</p>'
-            };
+//             // Message object
+//             let message = {
+//                 from: 'Sender Name <billing@bajalabruja.org>',
+//                 to: 'Recipient <pksubscriber@gmail.com>',
+//                 subject: 'Nodemailer is unicode friendly',
+//                 text: 'Hello to myself!',
+//                 html: '<p><b>Hello</b> to myself!</p>'
+//             };
 
-            transporter.sendMail(message, (err, info) => {
-                if (err) {
-                    console.log('Error occurred. ' + err.message);
-                    return process.exit(1);
-                }
+//             transporter.sendMail(message, (err, info) => {
+//                 if (err) {
+//                     console.log('Error occurred. ' + err.message);
+//                     return process.exit(1);
+//                 }
 
-                console.log('Message sent: %s', info.messageId);
-                // Preview only available when sending through an Ethereal account
-                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-            });
-        });
-    })();
-}); 
+//                 console.log('Message sent: %s', info.messageId);
+//                 // Preview only available when sending through an Ethereal account
+//                 console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//             });
+//         });
+//     })();
+// }); 
 
 
 // /*    Description: Delete Product Thumb
