80c78f6381ac5d6893666573195236df523c954b
PK <pksubscriber@gmail.com>
2022-08-30 09:46:58 -0400

    enforcing fix

--- public/javascripts/itemEdit.js
+++ public/javascripts/itemEdit.js
@@ -26,23 +26,23 @@ fetchItem = async() => {
     // });
 },
 updateItem = async() => {
-    console.log(Product);
-    // $.ajax({
-    //     url: '/updateProduct',
-    //     headers: {"Content-Type": "application/json"},
-    //     type: 'PUT',
-    //     dataType: 'json',
-    //     data: JSON.stringify(Product),
-    //     success: function (data, textStatus, xhr) {
-    //         //console.log(collection.products[0].productThumbs);
-    //         //location.href = "/product/" + itemId;
-    //         sendToastMessage("Changes saved successfully.");
-    //     },
-    //     error: function (xhr, textStatus, errorThrown) {
-    //         console.log('Error in Operation');
-    //         console.log('Error: ' + errorThrown);
-    //     }
-    // });
+    console.log(updateItem);
+    $.ajax({
+        url: '/updateProduct',
+        headers: {"Content-Type": "application/json"},
+        type: 'PUT',
+        dataType: 'json',
+        data: JSON.stringify(Product),
+        success: function (data, textStatus, xhr) {
+            //console.log(collection.products[0].productThumbs);
+            //location.href = "/product/" + itemId;
+            sendToastMessage("Changes saved successfully.");
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log('Error in Operation');
+            console.log('Error: ' + errorThrown);
+        }
+    });
 },
 buildImageArray = (element) => {
     var productStringArray = [];
