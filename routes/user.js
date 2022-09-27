388b4492cb31840c2f757536a37728cb6e356ca6
Phillip Knezevich <pksubscriber@gmail.com>
2022-09-27 19:38:55 -0400

    added forgot p/w function

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const uri = process.env.MONGO_DB;
+var SibApiV3Sdk = require("sib-api-v3-sdk");
 
 var csrf = require("csurf");
 var passport = require("passport");
@@ -118,6 +118,54 @@ router.put('/newUser', function (req, res) {
     })();
 });
 
+router.post('/emailTest', function (req, res) {
+    console.log('email test');
+    var defaultClient = SibApiV3Sdk.ApiClient.instance;
+    var apiKey = defaultClient.authentications["api-key"];
+    apiKey.apiKey = process.env.SENDINBLUE_SMTP_KEY;
+    var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+    var emailAddress = req.body.emailAddress;
+    console.log(emailAddress);
+    User.findOne({'email': emailAddress}, function (err, user) {
+        if(err) {
+            console.log(err);
+        }
+        if (!user) {
+            res.json('Unable to find user.')
+            console.log('Unable to find user.');
+        }
+        if (user) {
+            console.log(user);
+
+            sendSmtpEmail = {
+                sender: {
+                    email: "donotreply@bajalabruja.org",
+                    name: "bajalabruja.org"
+                },
+            to: [
+                {
+                    email: user.email,
+                    name: user.firstName + " " + user.lastName
+                },
+            ],
+            subject: "Password Reset",
+            htmlContent: EmailForgotPassword(user)
+            };
+            apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
+                console.log("API called successfully. Returned data: " + data);
+                res.sendStatus(200);
+            }, function (error) {
+                console.log(error);
+                console.error(error);
+            });
+            res.json(user);
+        }
+    });
+});
+
+
 module.exports = router;
 
 function isLoggedIn(req, res, next) {
@@ -132,4 +180,27 @@ function notLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/");
+}
+
+
+
+function EmailForgotPassword(user) {
+    var body = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+            <link rel="stylesheet" href="https://baja.a2hosted.com/fonts/stylesheet.css">
+        </head>
+        <body style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background-color:#000; color:#FFF; padding: 1rem;">
+            <h1> <img src="https://baja.a2hosted.com/images/baja_logo.png" style="max-height: 7rem;" alt="Baja la Bruja Logo"/> </h1>            
+            <h3>Forgot your password?</h3>
+            <p>Hi ${user.firstName},</p>
+            <p>Please use the following link to reset your password: <a style='color:white; font-size:larger' href="http://www.bajalabruja.org/user/passwordReset?refId=${user.authToken}">RESET MY PASSWORD</a></p>
+            <p>Thank you, <br> Baja La Bruja Team</p>
+        </body>
+    </html>    
+    `
+    return body;
 }
